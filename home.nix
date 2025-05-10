e8ea3d0048a9dc0fcbf3d3d662db1380d1c2346a
Bill Soudan <bill@soudan.net>
2025-05-09 22:41:00 -0400

    move comments

--- home.nix
+++ home.nix
@@ -4,28 +4,6 @@
   home.username = "bsoudan";
   home.homeDirectory = "/home/bsoudan";
 
-  # link the configuration file in current directory to the specified location in home directory
-  # home.file.".config/i3/wallpaper.jpg".source = ./wallpaper.jpg;
-
-  # link all files in `./scripts` to `~/.config/i3/scripts`
-  # home.file.".config/i3/scripts" = {
-  #   source = ./scripts;
-  #   recursive = true;   # link recursively
-  #   executable = true;  # make all files executable
-  # };
-
-  # encode the file content in nix configuration file directly
-  # home.file.".xxx".text = ''
-  #     xxx
-  # '';
-
-  # set cursor size and dpi for 4k monitor
-  #xresources.properties = {
-  #  "Xcursor.size" = 16;
-  #  "Xft.dpi" = 172;
-  #};
-
-  # Packages that should be installed to the user profile.
   home.packages = with pkgs; [
     # archives
     zip
@@ -169,3 +147,24 @@
   # Let home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
+
+  # link the configuration file in current directory to the specified location in home directory
+  # home.file.".config/i3/wallpaper.jpg".source = ./wallpaper.jpg;
+
+  # link all files in `./scripts` to `~/.config/i3/scripts`
+  # home.file.".config/i3/scripts" = {
+  #   source = ./scripts;
+  #   recursive = true;   # link recursively
+  #   executable = true;  # make all files executable
+  # };
+
+  # encode the file content in nix configuration file directly
+  # home.file.".xxx".text = ''
+  #     xxx
+  # '';
+
+  # set cursor size and dpi for 4k monitor
+  #xresources.properties = {
+  #  "Xcursor.size" = 16;
+  #  "Xft.dpi" = 172;
+  #};
